d30e288d8577fe37c2e5fdd48f608827b5f44e3d
tyson lind <tysonfloydlind@gmail.com>
2022-12-16 10:01:19 -0700

    complete

--- routes/products.route.js
+++ routes/products.route.js
@@ -7,7 +7,7 @@ router.get("/:id?", async (req, res, next) => {
   let { id } = req.params;
   let data;
 
-  if (id) {
+  if (id !== "all" || id !== "") {
     data = await products.findOne(id);
   } else {
     data = await products.findAll();
@@ -17,17 +17,17 @@ router.get("/:id?", async (req, res, next) => {
 });
 
 router.post("/", async (req, res, next) => {
-  let data = await products.findAll();
+  let data = await products.addOne();
   res.json(data);
 });
 
 router.put("/:id", async (req, res, next) => {
-  let data = await products.findAll();
+  let data = await products.updateOne();
   res.json(data);
 });
 
 router.delete("/:id", async (req, res, next) => {
-  let data = await products.findAll();
+  let data = await products.removeOne();
   res.json(data);
 });
 
